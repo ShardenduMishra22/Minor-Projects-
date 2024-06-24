1727970ff135c111da787db881c693678f885067
Shardendu Mishra <shardendumishra01@gmail.com>
2024-06-24 17:28:55 +0530

    Projecs 6 : Color Changer Infinity

--- Projects/6_Project  -  Unlimited Colors/index.js
+++ Projects/6_Project  -  Unlimited Colors/index.js
@@ -1,11 +1,30 @@
 // generate color
-const color = function(){
+function color(){
     const hex = "0123456789ABCDEF";
-    const clr = '#';
-    for(let i=0;i < 6;i++){
-        let num = Math.floor((mat.random()*16))
-        color += hex[num]; 
+    let clr = '#';  
+    for(let i = 0; i < 6; i++){
+        let num = Math.floor((Math.random() * 16));
+        clr += hex[num]; 
     }
     return clr;
 }
 
+let ChngClr;  
+
+function StartChangingColor(){
+    if(!ChngClr){
+        document.body.style.transition = "background-color 1s";
+        ChngClr = setInterval(ColorChange, 500);
+    }
+    function ColorChange(){
+        document.body.style.backgroundColor = color();  
+    }
+}
+
+const StopChangingColor = function(){
+    clearInterval(ChngClr);
+    ChngClr = null;
+}
+
+document.querySelector('.start').addEventListener('click', StartChangingColor);
+document.querySelector('.stop').addEventListener('click', StopChangingColor);
